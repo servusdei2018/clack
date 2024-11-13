ad2baabb84376f0567f85c0f421ca69cf046b468
Nathanael Bracy <njtbracy@protonmail.com>
2024-11-13 12:56:14 -0500

    tests: implement HelpMessageTest, LoginMessageTest
    Signed-off-by: Nathanael Bracy <njtbracy@protonmail.com>

---
+++ test/sparta/clack/message/HelpMessageTest.java
@@ -0,0 +1,41 @@
+package sparta.clack.message;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import static java.time.Duration.between;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelpMessageTest {
+
+    HelpMessage msg;
+    Instant now;
+
+    final String USERNAME = "the user";
+
+    @BeforeEach
+    void setUp() {
+        msg = new HelpMessage(USERNAME);
+    }
+
+    @Test
+    void getMsgType() {
+        assertEquals(MsgType.HELP, msg.getMsgType());
+    }
+
+    @Test
+    void getTimestamp() {
+        now = Instant.now();
+        Duration duration = between(msg.getTimestamp(), now);
+        long timeDiff = Math.abs(duration.toSeconds());
+        assertTrue(timeDiff <= 1);
+    }
+
+    @Test
+    void getUsername() {
+        assertEquals(USERNAME, msg.getUsername());
+    }
+}
---
+++ test/sparta/clack/message/LoginMessageTest.java
@@ -0,0 +1,47 @@
+package sparta.clack.message;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import static java.time.Duration.between;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginMessageTest {
+
+    LoginMessage msg;
+    Instant now;
+
+    final String USERNAME = "the user";
+    final String PASSWORD = "goldenKnights123";
+
+    @BeforeEach
+    void setUp() {
+        msg = new LoginMessage(USERNAME, PASSWORD);
+    }
+
+    @Test
+    void getMsgType() {
+        assertEquals(MsgType.LOGIN, msg.getMsgType());
+    }
+
+    @Test
+    void getTimestamp() {
+        now = Instant.now();
+        Duration duration = between(msg.getTimestamp(), now);
+        long timeDiff = Math.abs(duration.toSeconds());
+        assertTrue(timeDiff <= 1);
+    }
+
+    @Test
+    void getUsername() {
+        assertEquals(USERNAME, msg.getUsername());
+    }
+
+    @Test
+    void getPassword() {
+        assertEquals(PASSWORD, msg.getPassword());
+    }
+}
